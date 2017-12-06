9ea435e998d076125e4a08526b269a96789208fd
Tessmore <fabientesselaar@gmail.com>
2017-12-07 00:38:26 +0100

    Added day 5 & 6

--- day2/main.js
+++ day2/main.js
@@ -14,6 +14,28 @@
     In this example, the spreadsheet's checksum would be 8 + 4 + 6 = 18.
 
     What is the checksum for the spreadsheet in your puzzle input?
+
+
+    --- Part Two ---
+
+    "Great work; looks like we're on the right track after all. Here's a star for your effort." However, the program seems a little worried. Can programs be worried?
+
+    "Based on what we're seeing, it looks like all the User wanted is some information about the evenly divisible values in the spreadsheet. Unfortunately, none of us are equipped for that kind of calculation - most of us specialize in bitwise operations."
+
+    It sounds like the goal is to find the only two numbers in each row where one evenly divides the other - that is, where the result of the division operation is a whole number. They would like you to find those numbers on each line, divide them, and add up each line's result.
+
+    For example, given the following spreadsheet:
+
+    5 9 2 8
+    9 4 7 3
+    3 8 6 5
+
+    In the first row, the only two numbers that evenly divide are 8 and 2; the result of this division is 4.
+    In the second row, the two numbers are 9 and 3; the result is 3.
+    In the third row, the result is 2.
+    In this example, the sum of the results would be 4 + 3 + 2 = 9.
+
+    What is the sum of each row's result in your puzzle input?
 */
 
 const input = require('fs').readFileSync('input.txt', 'utf8');
@@ -40,12 +62,45 @@ function getRowSum(row) {
 }
 
 
+function isEven(num) {
+    return num % 1 === 0;
+}
+
+function getSpreadSum(row) {
+    var sum = 0;
+
+    for (var i=0; i < row.length; i++) {
+        for (var j=i+1; j < row.length; j++) {
+            var numA = Number(row[i]);
+            var numB = Number(row[j]);
+            var res = 0;
+
+            if (numA > numB) {
+                res = numA / numB;
+            }
+            else {
+                res = numB / numA;
+            }
+
+            if (isEven(res)) {
+                sum += res;
+            }
+        }
+    }
+
+    return sum;
+}
+
+
 var checksum = 0;
+var spreadsum = 0;
 
 for (var i=0; i < lines.length; i++) {
     var row = lines[i].split(/\s+/);
 
     checksum += getRowSum(row);
+    spreadsum += getSpreadSum(row);
 }
 
 console.log(checksum);
+console.log(spreadsum);
