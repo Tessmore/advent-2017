8cb3d7de6f3ce7c695d3a80fe485f755b734dca1
Tessmore <fabientesselaar@gmail.com>
2017-12-10 18:21:04 +0100

    Day 9

--- day8/main.js
+++ day8/main.js
@@ -24,6 +24,12 @@
   You might also encounter <= (less than or equal to) or != (not equal to). However, the CPU doesn't have the bandwidth to tell you what all the registers are named, and leaves that to you to determine.
 
   What is the largest value in any register after completing the instructions
+
+
+  --- Part Two ---
+
+  To be safe, the CPU also needs to know the highest value held in any register during this process so that it can decide how much memory to allocate to these operations. For example, in the above instructions, the highest value ever held was 10 (in register c after the third instruction was evaluated).
+
 */
 
 const input = require('fs').readFileSync('input.txt', 'utf8');
